2092e7f7f331d8c7f5ecb7f7e74f4e27f30eb403
maxdenuevo <maxihnen@gmail.com>
2024-06-17 21:43:16 -0400

    adding routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,19 @@
-import { useState } from 'react'
+
 import './App.css'
-import { Title } from '../src/components/Title'
-import { Button } from './components/Button'
-import  { TextoLibre } from './components/TextoLibre'
-import  { Contador } from './components/Counter'
-import { ListPokemon } from './components/Pokemon'
-import { RocketList } from './components/SpaceX'
-import { characterList } from './components/Characters'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HomePage } from './pages/HomePage'
+import { ContactPage } from './pages/ContactPage'
 
 
 function App() {
   return (
-    <div>
-    <Title nombre= "Max" apellido= "Ihnen"></Title>
-    <h3>Listen to the button:</h3>
-    <Button></Button>
-    <h3>clicker counter:</h3>
-    <Contador></Contador>
-    <h3>Copy text out of the text box</h3>
-    <TextoLibre></TextoLibre>
-    <h3>fetch from Pokémon API</h3>
-    <ListPokemon></ListPokemon>
-    <h3>fetch from SpaceX API</h3>
-    <RocketList></RocketList>
-    </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/home' element={<HomePage />} />
+            <Route path='/home/contact' element={<ContactPage/>} />
+        </Routes>
+      </BrowserRouter>
   );
 }
 
